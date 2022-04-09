f1ed62457620133c9a7e3a3c7530d24f407d0698
MrUniverse44 <oficial.mruniverse@gmail.com>
2022-04-09 12:05:42 -0500

    Added Comments to SlimeCommand, Updated to v1.0.4

--- src/main/java/dev/mruniverse/slimelib/commands/command/SlimeCommand.java
+++ src/main/java/dev/mruniverse/slimelib/commands/command/SlimeCommand.java
@@ -4,20 +4,40 @@ import dev.mruniverse.slimelib.commands.sender.Sender;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public interface SlimeCommand {
     List<String> array = new ArrayList<>();
 
+    /**
+     * This will be the main command for this command-class.
+     * @return String - Command
+     */
     String getCommand();
 
+    /**
+     * Add extra aliases to execute the command
+     * @return List - Aliases
+     */
     default List<String> getAliases() {
         return array;
     }
 
+    /**
+     * Execute the command
+     * @param sender the sender using the command
+     * @param commandLabel the command-alias used
+     * @param args arguments of the command executed
+     */
     void execute(@NotNull Sender sender, @NotNull String commandLabel, @NotNull String[] args);
 
+    /**
+     * Add Tab Complete to your command
+     * @param sender the sender using the command
+     * @param commandLabel the command-alias used
+     * @param args arguments of the tab complete
+     * @return List - the tab complete arguments
+     */
     @SuppressWarnings("unused")
     default List<String> onTabComplete(Sender sender, String commandLabel, String[] args) {
         return array;
