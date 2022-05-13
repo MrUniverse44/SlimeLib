ecf62175e50d781fc70de80147195cdce53dc4e6
MrUniverse44 <oficial.mruniverse@gmail.com>
2022-05-13 15:24:20 -0500

    Bug Fix

--- src/main/java/dev/mruniverse/slimelib/input/sponge/SpongeInputManager.java
+++ src/main/java/dev/mruniverse/slimelib/input/sponge/SpongeInputManager.java
@@ -1,19 +1,18 @@
 package dev.mruniverse.slimelib.input.sponge;
 
 import dev.mruniverse.slimelib.input.InputManager;
-import org.spongepowered.plugin.builtin.jvm.Plugin;
 
 import java.io.InputStream;
 
 public class SpongeInputManager implements InputManager {
-    private final Plugin plugin;
+    private final Class<?> plugin;
 
     public <T> SpongeInputManager(T plugin) {
-        this.plugin = (Plugin) plugin;
+        this.plugin = (Class<?>) plugin;
     }
 
     @Override
     public InputStream getInputStream(String resource) {
-        return plugin.getClass().getResourceAsStream(resource);
+        return plugin.getResourceAsStream(resource);
     }
 }
--- src/main/java/dev/mruniverse/slimelib/input/velocity/VelocityInputManager.java
+++ src/main/java/dev/mruniverse/slimelib/input/velocity/VelocityInputManager.java
@@ -1,19 +1,18 @@
 package dev.mruniverse.slimelib.input.velocity;
 
-import com.velocitypowered.api.plugin.Plugin;
 import dev.mruniverse.slimelib.input.InputManager;
 
 import java.io.InputStream;
 
 public class VelocityInputManager implements InputManager {
-    private final Plugin plugin;
+    private final Class<?> plugin;
 
     public <T> VelocityInputManager(T plugin) {
-        this.plugin = (Plugin) plugin;
+        this.plugin = (Class<?>) plugin;
     }
 
     @Override
     public InputStream getInputStream(String resource) {
-        return plugin.getClass().getResourceAsStream(resource);
+        return plugin.getResourceAsStream(resource);
     }
 }
